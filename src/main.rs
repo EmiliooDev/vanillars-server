9d176e541b70d840623d3967729a8af84796ee81
Emilio Ortiz <ortizemilio.dev@gmail.com>
2023-05-30 13:18:12 -0600

    Error handling

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,30 @@ fn main() -> std::io::Result<()> {
     println!("Server running on port 8080");
 
     for stream in listener.incoming() {
-        let mut stream = stream?;
+        let mut stream = match stream {
+            Ok(stream) => stream,
+            Err(e) => {
+                eprintln!("Error to accept the connection: {}", e);
+                continue;
+            }
+        };
+
         let mut buffer = [0; 1024];
-        stream.read(&mut buffer)?;
+        let bytes_read = match stream.read(&mut buffer) {
+            Ok(bytes_read) => bytes_read,
+            Err(e) => {
+                eprintln!("Error to read the data: {}", e);
+                continue;
+            }
+        };
+
         let response = "HTTP/1.1 200 OK\r\n\r\nHello World!";
-        stream.write_all(response.as_bytes())?;
+        match stream.write_all(response.as_bytes()) {
+            Ok(_) => (),
+            Err(e) => {
+                eprintln!("Error to send the response: {}", e);
+            }
+        };
     }
     Ok(())
 }
